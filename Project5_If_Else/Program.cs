8dec72ff26083a8a25aa001753b6257bd8311899
Dmitry Korotchuk <dmitrykorotchuk@mail.ru>
2023-01-31 23:29:57 +0300

    Project 5. If and Else

---
+++ Project5_If_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿// If и Else
+Console.WriteLine("Укажите Ваше имя: ");
+string username = Console.ReadLine();
+if (username.ToLower()=="маша")
+{
+  Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+  Console.Write("Привет, ");
+  Console.WriteLine(username);
+}
